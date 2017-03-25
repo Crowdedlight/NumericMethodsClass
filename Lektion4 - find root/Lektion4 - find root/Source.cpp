6611e4ed23377f0fb6389e1e8178d191c7097bf9
Crowdedlight <crowdedhd@gmail.com>
2017-03-25 19:18:00 +0100

    finished exercise for bisection and regular falsi

--- Lektion4 - find root/Lektion4 - find root/Source.cpp
+++ Lektion4 - find root/Lektion4 - find root/Source.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include "utilities.h"
 #include <math.h>
-//#include "roots.h"
 
 using namespace std;
 using namespace util;
@@ -52,6 +51,9 @@ void printTable(vector<vector<Doub>> colums, vector<string> headers)
 		}
 		cout << endl;		
 	}
+
+    //End spacing 
+    cout << endl << endl << endl;
 }
 
 ///Bisection
@@ -93,16 +95,103 @@ Doub rtbis(T &func, const Doub x1, const Doub x2, const Doub xacc) {
 		//X_k
 		xk.push_back(rtb);
 		//D_k
-		if (j == 0) dk.push_back(abs(x1-x2));
-		else dk.push_back(dx);	
-		//C := xi+1 - x / x - xi-1
-		c.push_back((dx*0.5)/dx);
+		if (j == 0) dk.push_back(0);
+		else dk.push_back(xk[j] - xk[j - 1]);
+        //C := xi - x / x-1 - xi-2
+        if (j < 2)
+        {
+            c.push_back(0);
+        }
+        else
+        {
+            Doub cCal = (xk[j] - xk[j - 1]) / (xk[j - 1] - xk[j - 2]);
+            c.push_back((cCal));
+        }
 
 	}
 	throw("Too many bisections in rtbis");
 }
 
+///False Position
+template <class T>
+Doub rtflsp(T &func, const Doub x1, const Doub x2, const Doub xacc) {
+    const Int MAXIT = 30;
+    Doub xl, xh, del;
+    Doub fl = func(x1);
+    Doub fh = func(x2);
+    if (fl*fh > 0.0) throw("Root must be bracketed in rtflsp");
+    if (fl < 0.0) {
+        xl = x1;
+        xh = x2;
+    }
+    else {
+        xl = x2;
+        xh = x1;
+        SWAP(fl, fh);
+    }
+    Doub dx = xh - xl;
+
+    //table vectors colums
+    vector<string> headers = { "k", "Xk", "Dk", "C" };
+    vector<Doub> indexes;
+    vector<Doub> xk;
+    vector<Doub> dk;
+    vector<Doub> c;
+    vector<vector<Doub>> tableColums;
+
+
+    for (Int j = 0; j < MAXIT; j++) {
+        Doub rtf = xl + dx*fl / (fl - fh);
+        Doub f = func(rtf);
+        if (f < 0.0) {
+            del = xl - rtf;
+            xl = rtf;
+            fl = f;
+        }
+        else {
+            del = xh - rtf;
+            xh = rtf;
+            fh = f;
+        }
+        dx = xh - xl;
+
+        //Table values
+        indexes.push_back(j);
+        //X_k
+        xk.push_back(rtf);
+        //D_k
+        if (j == 0) dk.push_back(0);
+        else dk.push_back(xk[j] - xk[j-1]);
+        //C := xi - x / x-1 - xi-2
+        if (j < 2)
+        {
+            c.push_back(0);
+        }
+        else
+        {
+            Doub cCal = (xk[j] - xk[j - 1]) / (xk[j - 1] - xk[j - 2]);
+            c.push_back((cCal));
+        }
+
+
+        //End check
+        if (abs(del) < xacc || f == 0.0)
+        {
+            //print vetor
+            tableColums.push_back(indexes);
+            tableColums.push_back(xk);
+            tableColums.push_back(dk);
+            tableColums.push_back(c);
+            printTable(tableColums, headers);
+            return rtf;
+        }
+    }
+    throw("Maximum number of iterations exceeded in rtflsp");
+}
+
 #pragma endregion 
+
+
 
 
 int main()
@@ -112,7 +201,19 @@ int main()
 	double x2 = PI / 2;
 	double acc1 = pow(10, -8);
 
+    //Bisection
+    cout << "----------------------" << endl;
+    cout << "------ BISECTION -----" << endl;
+    cout << "----------------------" << endl;
 	auto result = rtbis(f_1, x1, x2, acc1);
+
+    //False position - Regular falsi
+    double acc2 = pow(10, -16);
+    cout << "----------------------" << endl;
+    cout << "--- Regular Falsi ----" << endl;
+    cout << "----------------------" << endl;
+    auto res2 = rtflsp(f_1, x1, x2, acc2);
+
 
 	system("pause");
 	return 0;
