cb04bb2a342fc48e6dfef1360aa688ef2066ded3
Crowdedlight <crowdedhd@gmail.com>
2017-04-13 15:51:06 +0200

    wip of c-function

--- mandatory2/mandatory2/Source.cpp
+++ mandatory2/mandatory2/Source.cpp
@@ -7,29 +7,59 @@
 using namespace std;
 using namespace util;
 
+#pragma region headers
+
 template<class T>
 Doub trapez(T &func, Doub a, Doub b, Doub acc);
 
-struct func_test
-{
-	func_test() {}
-	Doub operator()(Doub x) { return ; }
+template <class T>
+double crossSection(T &func, T &GAMMA, double &t, double &y, double &z);
+
+#pragma endregion 
+
+#pragma region Functions
+const double a = 1;
+const double b = 1;
+const double v = 1;
+const double k = 1;
+const double w = 1;
+const double rho = 1;
+
+struct gamma {
+	gamma() {};
+	double operator()(double x, double y, double z) { return pow((pow(x, 2) / (pow(a, 2) * pow(z, 2))) + (pow(y, 2) / (pow(b, 2) * pow(z, 2))) + 1, -0.5); }
 };
+struct GAMMA {
+	GAMMA() {};
+	double operator()(double t, double y, double z) { return pow(((pow(v, 2) * pow(t, 2)) / (pow(a, 2) * pow(z, 2))) + (pow(y, 2) / (pow(b, 2) * pow(z, 2))) + 1, -0.5); }
+};
+struct func_gamma {
+	func_gamma() {};
+	double operator()(double gamma) { return (1 - (pow(1 - (gamma), 2) / (3 / 2 - sqrt(2)))) * cos(k * pow(1 - (gamma), 2)); }
+};
+struct func_GAMMA {
+	func_GAMMA() {};
+	double operator()(double GAMMA) { return (1 - (pow(1 - GAMMA, 2) / (3 / 2 - sqrt(2)))) * cos(k * pow(1 - GAMMA, 2)); }
+};
+struct alpha {
+	alpha() {};
+	double operator()(double y, double z) { return (a*z / v) * sqrt(1 - (pow(y, 2) / (pow(b, 2)*pow(z, 2)))); }
+};
+#pragma endregion 
 
 int main()
 {
 	Doub acc = pow(10, -3);
 
-	func_test f1;
-
-	cout << trapez(f1, 0, 4, acc) << endl;;
-
+	GAMMA G;
+	func_GAMMA fG;
+	
 
 	system("pause");
 	return 0;
 }
 
-
+#pragma region trapez
 template<class T>
 Doub trapez(T &func, Doub a, Doub b, Doub acc)
 {
@@ -65,3 +95,22 @@ Doub trapez(T &func, Doub a, Doub b, Doub acc)
 
 	}
 }
+
+#pragma endregion 
+
+#pragma region crossSection
+template <class T>
+double crossSection(T &func, T &GAMMA, double &t, double &y, double &z) {
+	if (abs(y) > b*z)
+		return 0;
+	alpha a;
+	double a_integralBorder = a(y, z);
+	double b_integralBorder = -a(y, z);
+
+	// Call trapetz on: (w/pow(z,2) * pow(GAMMA,3) * func(GAMMA) , a_integralBorder, b_integralborder)
+	Doub res = trapez(w / pow(z, 2) * pow(GAMMA, 3) * func(GAMMA), a_integralBorder, b_integralBorder);
+
+	res = res / rho;
+	return res;
+}
+#pragma endregion 
